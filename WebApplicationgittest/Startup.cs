5aeab948e36c8b5c306927a0128053fcef1c9caf
EeNiArT <34338278+EeNiArT@users.noreply.github.com>
2018-09-25 23:02:29 +0500

    Add project files.

---
+++ WebApplicationgittest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationgittest.Startup))]
+namespace WebApplicationgittest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
